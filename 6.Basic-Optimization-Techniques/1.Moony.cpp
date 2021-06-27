77637a076ef33d8c3182a8cb5d5f35fef131ee0f
purplevarun <purplevarun@gmail.com>
2021-06-28 04:22:07 +0530

    1.Moony.cpp

--- 6.Basic-Optimization-Techniques/1.Moony.cpp
+++ 6.Basic-Optimization-Techniques/1.Moony.cpp
@@ -1,4 +1,6 @@
 // Initial Thoughts : Looks like Maximum Sum of Subarray of Size 3
+// PS : Wrong
+// Actual Logic is Easier. Does not involve Sliding Window Concept. Bleh.
 #include <bits/stdc++.h>
 #define int int64_t 
 using namespace std;
@@ -9,16 +11,15 @@ void Onigiri(){
 	for (int i=0;i<N;++i){
 		cin >> A[i];
 	}
-	int K = 3;
-	int maxSum = 0;
-	// first K students
-	for (int i=0;i<K;++i){
-		maxSum += A[i];
+	int maxSum = -1e9, currentSum;
+	for (int i=1;i<N-1;++i){
+		currentSum = A[i-1] + A[i] + A[i+1];
+		maxSum = max (maxSum, currentSum);
 	}
-	int currentSum = maxSum;
-	for (int i=K;i<N;++i){
-		
-	}
+	// there are 2 more possibilities.
+	maxSum = max (maxSum, A[0] + A[1] + A[N-1]);
+	maxSum = max (maxSum, A[0] + A[N-1] + A[N-2]);
+	cout << maxSum << endl;
 }
 int32_t main () {
 	ios_base::sync_with_stdio(0);
