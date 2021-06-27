688302330a8c531ee4c6477ef6fa2066023407a1
purplevarun <purplevarun@gmail.com>
2021-06-28 01:00:19 +0530

    13.Triangle-With-Angle.cpp

---
+++ 0.Easy-Problems-to-Get-Started/13.Triangle-With-Angle.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define int int64_t 
+using namespace std;
+int32_t main () {
+	int A, B, C; cin >> A >> B >> C;
+	if ((A + B + C) == 180)
+		cout << "YES" << endl;
+	else 
+		cout << "NO" << endl;
+}
