12f14704df820ca955f765bd8aee13af545e4354
purplevarun <purplevarun@gmail.com>
2021-06-28 03:39:19 +0530

    1.Moony.cpp

---
+++ 6.Basic-Optimization-Techniques/1.Moony.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int int64_t 
+using namespace std;
+void Onigiri(){
+	
+}
+int32_t main () {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int T; cin >> T;
+	while (T--) Onigiri();
+}
