606f7fbef627bf4743a75ef0492103d3c6ffe303
purplevarun <purplevarun@gmail.com>
2021-06-28 03:45:21 +0530

    1.Moony.cpp

--- 6.Basic-Optimization-Techniques/1.Moony.cpp
+++ 6.Basic-Optimization-Techniques/1.Moony.cpp
@@ -1,8 +1,24 @@
+// Initial Thoughts : Looks like Maximum Sum of Subarray of Size 3
 #include <bits/stdc++.h>
 #define int int64_t 
 using namespace std;
 void Onigiri(){
-	
+	int N;
+	cin >> N;
+	int A[N];
+	for (int i=0;i<N;++i){
+		cin >> A[i];
+	}
+	int K = 3;
+	int maxSum = 0;
+	// first K students
+	for (int i=0;i<K;++i){
+		maxSum += A[i];
+	}
+	int currentSum = maxSum;
+	for (int i=K;i<N;++i){
+		
+	}
 }
 int32_t main () {
 	ios_base::sync_with_stdio(0);
