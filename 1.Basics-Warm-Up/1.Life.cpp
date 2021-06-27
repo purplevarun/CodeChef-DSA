4e9f1c9065c270697a466e84b7200052433f1d12
purplevarun <purplevarun@gmail.com>
2021-06-28 02:31:11 +0530

    1.Life.cpp

---
+++ 1.Basics-Warm-Up/1.Life.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int int64_t 
+using namespace std;
+int32_t main () {
+	int X;
+	while (true){
+		cin >> X;
+		if (X == 42)
+			return 0;
+		cout << X << endl;
+	}
+}
