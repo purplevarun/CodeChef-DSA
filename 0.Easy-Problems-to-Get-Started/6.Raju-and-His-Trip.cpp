3b913f0d89c5b026fa03a45a20db4b171b6fb2b7
purplevarun <purplevarun@gmail.com>
2021-06-28 00:38:49 +0530

    6.Raju-and-His-Trip.cpp

---
+++ 0.Easy-Problems-to-Get-Started/6.Raju-and-His-Trip.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define int int64_t 
+using namespace std;
+int32_t main () {
+	int N; cin >> N;
+	if (N%5==0 || N%6==0){
+		cout << "YES\n";
+	}
+	else 
+		cout << "NO\n";
+}
