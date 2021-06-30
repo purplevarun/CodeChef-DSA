f1c120cf8d4f3666f8abc0c4d3335081f0ba1214
purplevarun <purplevarun@gmail.com>
2021-06-30 18:21:36 +0530

    3.Chef-and-Weird-Queries.cpp

--- 6.Basic-Optimization-Techniques/3.Chef-and-Weird-Queries.cpp
+++ 6.Basic-Optimization-Techniques/3.Chef-and-Weird-Queries.cpp
@@ -4,13 +4,10 @@ using namespace std;
 void onigiri() {
 	int x; cin >> x;
 	int cnt = 0;
-	for (int i=1;i*i<=x;++i){
-		for (int j=1;j<=700;++j){
-			if (i*i + j <= x)
-				cnt ++;
-		}
+	for (int i=1;i<=700 && i < x; i++){
+		cnt += sqrt(x - i);
 	}
-	cout << cnt << endl;
+	cout << cnt << '\n';
 }
 int32_t main () {
 	ios_base::sync_with_stdio(0);
