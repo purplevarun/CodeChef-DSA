f2037a6cef79fa320a0a225e2341c63631be1fe3
purplevarun <purplevarun@gmail.com>
2021-07-01 11:20:39 +0530

    backup

---
+++ 6.Basic-Optimization-Techniques/5.Help-Patra-Bhaiya.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+const int MOD = 1e9 + 7;
+int N, K;
+void onigiri() {
+	cin >> N >> K;
+	
+}
+int32_t main () {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+	cin >> t;
+	while (t--){
+		onigiri ();
+	}
+}
