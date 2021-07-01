fd33d26c10feb3ebe5feccceac4d09d1192af610
purplevarun <purplevarun@gmail.com>
2021-07-01 20:29:48 +0530

    Readme.MD

---
+++ 1.Basics-Warm-Up/7.Laddu.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+void onigiri() {
+	int n;
+	string org;
+	cin >> n >> org;
+	int laddus = 0;
+	while (n--){
+		string x;
+		cin >> x;
+		if (x == "CONTEST_WON"){
+			int Rank; cin >> Rank;
+			if (Rank <= 20)
+				laddus += 300 + (20 - Rank);
+			else 
+				laddus += 300;
+		}
+		else if (x == "TOP_CONTRIBUTOR"){
+			laddus += 300;
+		}
+		else if (x == "BUG_FOUND"){
+			int severity; cin >> severity;
+			laddus += severity;
+		}
+		else if (x == "CONTEST_HOSTED"){
+			laddus += 50;
+		}
+	}
+	//cout << laddus << endl;
+	if (org == "INDIAN"){
+		cout << laddus/200 << endl;
+	}
+	else 
+		cout << laddus/400 << endl;
+}
+int32_t main () {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+	cin >> t;
+	while (t--){
+		onigiri ();
+	}
+}
---
+++ 1.Basics-Warm-Up/8.Multiple-of-3.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+void onigiri() {
+	int K, A, B;
+	cin >> K >> A >> B;
+	
+}
+int32_t main () {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+	cin >> t;
+	while (t--){
+		onigiri ();
+	}
+}
