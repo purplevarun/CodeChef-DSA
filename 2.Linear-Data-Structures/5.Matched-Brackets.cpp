430eaca1a62d149e2f30e1259b4b75808785cfe5
purplevarun <purplevarun@gmail.com>
2021-07-03 04:07:43 +0530

    5.Matched-Brackets.cpp

---
+++ 2.Linear-Data-Structures/5.Matched-Brackets.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+void onigiri() {
+	int N; cin >> N;
+	int X;
+	int cnt = 0, maxCnt = -1e9, cntLoc = 0;
+	int depth = 0, maxDepth = -1e9, depthLoc = 0;
+	for (int i=1;i<=N;++i){
+		cin >> X;
+		if (X == 1)
+			cnt ++;
+		else 
+			cnt --;
+
+	}
+}
+int32_t main () {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	onigiri();
+}
