ef42c27632ec7ba705b428922b781727a134d587
purplevarun <purplevarun@gmail.com>
2021-07-03 00:24:51 +0530

    2.Linear-Data-Structures\3.Stupid-Machine.cpp

--- 2.Linear-Data-Structures/3.Stupid-Machine.cpp
+++ 2.Linear-Data-Structures/3.Stupid-Machine.cpp
@@ -2,13 +2,14 @@
 #define int int64_t
 using namespace std;
 void onigiri() {
+	
 }
 int32_t main () {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int t = 1;
-	cin >> t;
-	while (t--){
-		onigiri ();
-	}
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        onigiri ();
+    }
 }
