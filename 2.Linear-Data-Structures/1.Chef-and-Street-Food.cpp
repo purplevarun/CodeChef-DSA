11dbc08979616a1e21fcd05b44c041bad8f43530
purplevarun <purplevarun@gmail.com>
2021-07-02 11:51:09 +0530

    1.Chef-and-Street-Food.cpp

--- 2.Linear-Data-Structures/1.Chef-and-Street-Food.cpp
+++ 2.Linear-Data-Structures/1.Chef-and-Street-Food.cpp
@@ -2,7 +2,16 @@
 #define int int64_t
 using namespace std;
 void onigiri() {
-			
+	int N, S, P, V;
+	cin >> N;
+	int Max = -1e9;
+	while (N --){
+		cin >> S >> P >> V;
+		int customers = P / (S + 1);
+		int Sales = customers * V;
+		Max = max (Max, Sales);
+	}
+	cout << Max << '\n';
 }
 int32_t main () {
 	ios_base::sync_with_stdio(0);
