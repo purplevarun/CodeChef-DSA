24be375047fa88d49958cea639fa41b270a3dfce
purplevarun <purplevarun@gmail.com>
2021-07-03 15:36:39 +0530

    6.Infix-To-Postfix.cpp

--- 2.Linear-Data-Structures/6.Infix-To-Postfix.cpp
+++ 2.Linear-Data-Structures/6.Infix-To-Postfix.cpp
@@ -3,10 +3,21 @@
 using namespace std;
 int N;
 string S;
+int precedence (char ch){
+	if (ch == '^')
+		return 5;
+	else if (ch == '/')
+		return 4;
+	else if (ch == '*')
+		return 3;
+	else if (ch == '+' || ch == '-')
+		return 2;
+	return 1;
+}
 void onigiri() {
 	cin >> N >> S;
 	stack <char> st;
-		
+			
 }
 int32_t main () {
 	ios_base::sync_with_stdio(0);
