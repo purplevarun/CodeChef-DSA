268c9047670fedd2b78af7b7c12f622cfc40e67d
purplevarun <purplevarun@gmail.com>
2021-07-03 15:31:24 +0530

    6.Infix-To-Postfix.cpp

---
+++ 2.Linear-Data-Structures/6.Infix-To-Postfix.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+int N;
+string S;
+void onigiri() {
+	cin >> N >> S;
+	stack <char> st;
+		
+}
+int32_t main () {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+	cin >> t;
+	while (t--){
+		onigiri ();
+	}
+}
