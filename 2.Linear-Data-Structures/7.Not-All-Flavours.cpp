21eda237745a97268a766e6e5570804e7997c028
purplevarun <purplevarun@gmail.com>
2021-07-05 02:27:27 +0530

    2.Linear-Data-Structures\7.Not-All-Flavours.cpp

---
+++ 2.Linear-Data-Structures/7.Not-All-Flavours.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define ll long long
+using namespace std;
+int main () {
+    int t; cin >> t;
+    int N, K;
+    int A[100007];
+    while (t--){
+        cin >> N >> K;
+        for (int i=0;i<N;++i)
+            cin >> A[i];
+
+        
+    }
+}
