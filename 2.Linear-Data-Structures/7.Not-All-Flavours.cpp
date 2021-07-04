9ced44d0136f71b3723d296c3efc8e5292de8e21
purplevarun <purplevarun@gmail.com>
2021-07-05 02:48:23 +0530

    2.Linear-Data-Structures\7.Not-All-Flavours.cpp

--- 2.Linear-Data-Structures/7.Not-All-Flavours.cpp
+++ 2.Linear-Data-Structures/7.Not-All-Flavours.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <cstring>
-#define ll long long
+#include <unordered_map>
+#define int long long
 using namespace std;
-int main () {
+int32_t main () {
     int t; cin >> t;
-    int N, K;
-    int A[100007];
-    int Flavours[100007];
     while (t--){
+        int N, K;
         cin >> N >> K;
-        memset (Flavours, 0, sizeof(Flavours));
+        int A[N], dp[N]; // linear dp array to store length of subarrays at each index i
         for (int i=0;i<N;++i)
             cin >> A[i];
         
