63869f6cccb5411b5ada9f04184186b6b9246e06
purplevarun <purplevarun@gmail.com>
2021-07-05 02:29:01 +0530

    2.Linear-Data-Structures\7.Not-All-Flavours.cpp

--- 2.Linear-Data-Structures/7.Not-All-Flavours.cpp
+++ 2.Linear-Data-Structures/7.Not-All-Flavours.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <cstring>
 #define ll long long
 using namespace std;
 int main () {
     int t; cin >> t;
     int N, K;
     int A[100007];
+    int Flavours[100007];
     while (t--){
         cin >> N >> K;
+        memset (Flavours, 0, sizeof(Flavours));
         for (int i=0;i<N;++i)
             cin >> A[i];
-
         
     }
 }
