e1c77fee95dcc1e27e4b526d460350b541836c7b
purplevarun <purplevarun@gmail.com>
2021-07-06 21:45:54 +0530

    1.Save-Konoha.cpp

--- 3.STL/1.Save-Konoha.cpp
+++ 3.STL/1.Save-Konoha.cpp
@@ -2,9 +2,12 @@
 #define int int64_t
 using namespace std;
 void dbg (auto S){
+	#ifndef VarunDebug
+	return;
+	#endif
 	cout << "Set : ";
-	for (int i:S) 
-		cout << i << ' '; 
+	for (int i:S)
+		cout << i << ' ';
 	cout << '\n';
 }
 void Onigiri () {
@@ -13,10 +16,23 @@ void Onigiri () {
 	for (int i=0;i<N;++i)
 		cin >> A[i];
 	int cntr = 0;
-	multiset <int> dp;
+	multiset <int, greater<int>> dp;
 	for (int i=0;i<N;++i)
 		dp.insert (A[i]);
-	
+	dbg (dp);
+	while (Z > 0 && dp.size() > 0){
+		int Largest = *dp.begin();
+		dp.erase(dp.begin());
+		Z -= Largest;
+		Largest /= 2;
+		if (Largest > 0)
+			dp.insert (Largest);
+		cntr++;
+	}
+	if (Z > 0)
+		cout << "Evacuate" << '\n';
+	else
+		cout << cntr << '\n';
 
 }
 int32_t main (){
