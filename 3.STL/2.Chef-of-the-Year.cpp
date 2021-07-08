37677983e88f91406d928aedced4fab85a45e783
purplevarun <purplevarun@gmail.com>
2021-07-08 10:04:59 +0530

    2.Chef-of-the-Year.cpp

---
+++ 3.STL/2.Chef-of-the-Year.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+void Onigiri () {
+	int N, M; cin >> N >> M;
+
+}
+int32_t main (){
+	#ifndef VarunDebug
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	#endif
+
+	int t = 1;
+	//cin >> t;
+
+	for (int i=0;i<t;++i)
+		Onigiri();
+}
