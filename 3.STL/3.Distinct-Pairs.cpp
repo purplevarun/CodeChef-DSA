08576507555bde9f7e54e1a0936fee7ecc5d2cb4
purplevarun <purplevarun@gmail.com>
2021-07-11 02:33:12 +0530

    3.Distinct-Pairs.cpp

--- 3.STL/3.Distinct-Pairs.cpp
+++ 3.STL/3.Distinct-Pairs.cpp
@@ -1,13 +1,27 @@
 #include <bits/stdc++.h>
-#define ll long long
+#define int int64_t
 using namespace std;
 void Onigiri() {
 	int N, M; cin >> N >> M;
-	int A[N], B[N];
-	for (int i-0;i<N;++i) cin >> A[i]; 
-	for (int i-0;i<M;++i) cin >> B[i];
+	int A[N], B[M];
+	for (int i=0;i<N;++i) 
+		cin >> A[i]; 
+	for (int i=0;i<M;++i) 
+		cin >> B[i];
+	vector <pair <int, int>> Ans;
+	int maxPosA = max_element (A, A+N) - A; // position of max element of A
+	int minPosB = min_element (B, B+M) - B; // position of min element of B
+	for (int i=0;i<M;++i){
+		Ans.push_back ({maxPosA, i}); // pairing maxA with all elements of B
+	}
+	for (int j=0;j<N;++j){
+		if (j != maxPosA)
+		Ans.push_back ({j, minPosB}); // pairing all elements of A with minB
+	}
+	for (auto i:Ans)
+		cout << i.first << ' ' << i.second << '\n';
 }
-int main () {
+int32_t main () {
 	#ifndef VarunDebug
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);cout.tie(0);
